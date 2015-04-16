e1e2ea465d82a5281b5e9f91a658b5effa6ff02b
stevenmiller888 <stevenmiller888@me.com>
2015-04-16 11:19:26 -0700

    updated test/index.js

--- test/index.js
+++ test/index.js
@@ -17,16 +17,24 @@ describe('MapReduce()', function(){
     assert.equal(typeof MapReduce, 'function');
   });
 
-  it('should take an optional number of CPUs', function(){
+  it('should allow an optional value for parallelism', function(){
     var mapReduce = MapReduce(2);
     assert(mapReduce);
   });
 
-  it('should only allow positive values for the number of CPUs', function(){
+  it('should only allow positive values for parallelism', function(){
     try {
       MapReduce(-1);
     } catch (e) {
       assert.equal(e.message, 'The number of CPUs must be positive');
+    }
+  });
+
+  it('should only allow values less than the machine\'s number of CPUs for parallelism', function(){
+    try {
+      MapReduce(10);
+    } catch (e) {
+      assert.equal(e.message, 'The number of CPUs must be less than or equal to the machine\'s');
     }
   });
 
@@ -35,19 +43,27 @@ describe('MapReduce()', function(){
     assert.equal(typeof mapReduce, 'function');
   });
 
-  it('should work when the number of lines equals the number of CPUs', function(){
+  it('should work when the number of lines is less than the number of CPUs', function(){
     var mapReduce = MapReduce();
-    var article = fs.readFileSync('test/short.txt', { encoding: 'utf8' });
+    var article = fs.readFileSync('test/small.txt', { encoding: 'utf8' });
     mapReduce(article, helpers.map, helpers.reduce, function(res){
-      assert.deepEqual(res, helpers.shortResult);
+      assert.deepEqual(res, helpers.smallResult);
+    });
+  });
+
+  it('should work when the number of lines is equal to the number of CPUs', function(){
+    var mapReduce = MapReduce();
+    var article = fs.readFileSync('test/medium.txt', { encoding: 'utf8' });
+    mapReduce(article, helpers.map, helpers.reduce, function(res){
+      assert.deepEqual(res, helpers.mediumResult);
     });
   });
 
   it('should work when the number of lines is greater than the number of CPUs', function(){
     var mapReduce = MapReduce();
-    var article = fs.readFileSync('test/long.txt', { encoding: 'utf8' });
+    var article = fs.readFileSync('test/large.txt', { encoding: 'utf8' });
     mapReduce(article, helpers.map, helpers.reduce, function(res){
-      assert.deepEqual(res, helpers.longResult);
+      assert.deepEqual(res, helpers.largeResult);
     });
   });
 });
