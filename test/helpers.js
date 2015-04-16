f69fa02ee379a60206e6e707317dd9db64d3ac69
stevenmiller888 <stevenmiller888@me.com>
2015-04-15 20:04:05 -0700

    updated various files

--- test/helpers.js
+++ test/helpers.js
@@ -21,7 +21,7 @@ exports.map = function map(str){
  */
 
 exports.reduce = function(k, vals){
-  if (!vals.length) return 0;
+  if (!vals.length) return 1;
 
   return vals.reduce(function(acc, val){
     return acc + val;
@@ -95,4 +95,73 @@ exports.shortResult = {
   whose: 0,
   chained: 0,
   together: 0
+};
+
+/**
+ * Short result object.
+ */
+
+exports.longResult = {
+  execution: 4,
+  in: 12,
+  computer: 5,
+  and: 1,
+  software: 1,
+  engineering: 1,
+  is: 6,
+  the: 19,
+  process: 3,
+  by: 1,
+  which: 2,
+  a: 9,
+  or: 3,
+  virtual: 1,
+  machine: 3,
+  performs: 1,
+  instructions: 5,
+  of: 7,
+  program: 5,
+  trigger: 1,
+  sequences: 1,
+  simple: 1,
+  actions: 3,
+  on: 1,
+  executing: 1,
+  those: 1,
+  produce: 1,
+  effects: 1,
+  according: 1,
+  to: 1,
+  semantics: 1,
+  programs: 3,
+  for: 1,
+  may: 3,
+  execute: 1,
+  batch: 1,
+  without: 1,
+  human: 1,
+  interaction: 1,
+  user: 1,
+  type: 1,
+  commands: 3,
+  an: 2,
+  interactive: 1,
+  session: 1,
+  interpreter: 1,
+  this: 1,
+  case: 1,
+  are: 1,
+  simply: 1,
+  whose: 1,
+  chained: 1,
+  together: 1,
+  term: 1,
+  run: 1,
+  used: 1,
+  almost: 1,
+  synonymously: 1,
+  context: 1,
+  takes: 1,
+  place: 1,
+  crucial: 1
 };
